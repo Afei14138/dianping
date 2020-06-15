0738e0d6accf33e2fb47dc92791626a72ac557e9
BeaHero <625592378@qq.com>
2020-06-15 17:13:48 +0800

    登入登出以及获取当前用户功能

---
+++ src/main/java/com/afei/dianping/request/LoginReq.java
@@ -0,0 +1,32 @@
+/**
+ * @Description: 登录前端的请求$
+ * @Author: wpf
+ * @Date: $
+ */
+package com.afei.dianping.request;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginReq {
+    @NotBlank(message = "手机号不能为空")
+    private String telphone;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
